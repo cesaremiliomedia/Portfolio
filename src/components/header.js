c3e91797e3b14423c2ec1c94aca43ab969f24247
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-01-27 17:40:39 -0600

    header

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,11 @@ export default () => (
         href="https://fonts.googleapis.com/css?family=Bangers&display=swap"
         rel="stylesheet"
       />
+      <link
+        href="//db.onlinewebfonts.com/c/8bd07a0c3254bd96405a228a802b2083?family=flottflott"
+        rel="stylesheet"
+        type="text/css"
+      />
 
       <h1>Cesar Emilio Media</h1>
       <nav>
