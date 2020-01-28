9ba6b239ce0a4a09fba2006e78b45d318f965db7
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-01-27 18:28:04 -0600

    123dafsldjaf

--- src/components/layout2.js
+++ src/components/layout2.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
-import "./fonts/fonts.css"
+
 import "./layout.css"
 export default ({ children }) => (
   <div>
