40a63ffa5f1e4182fd0514cc0293eeae260efd8d
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-03-06 15:14:07 -0600

    poppins font

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,10 @@ export default () => (
         href="//db.onlinewebfonts.com/c/8bd07a0c3254bd96405a228a802b2083?family=flottflott"
         rel="stylesheet"
       />
+      <link
+        href="https://fonts.googleapis.com/css?family=Poppins"
+        rel="stylesheet"
+      ></link>
 
       <h1>Cesar Emilio Media</h1>
       <nav>
